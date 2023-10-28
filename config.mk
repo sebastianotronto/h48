f2dd7feb819fe4a8a4181dd1d04488e31f4b63a1
Sebastiano Tronto <sebastiano@tronto.net>
2023-10-28 22:15:31 +0200

    Removed config.mk

--- config.mk
+++
@@ -1,6 +0,0 @@
-CUBETYPE = CUBE_
-
-CFLAGS = -std=c99 -pthread -pedantic -Wall -Wextra -Wno-unused-parameter -O3
-DBGFLAGS = -DDEBUG -std=c99 -pthread -pedantic -Wall -Wextra -Wno-unused-parameter -Wno-unused-function -g3 -fsanitize=address -fsanitize=undefined
-
-CC = cc
