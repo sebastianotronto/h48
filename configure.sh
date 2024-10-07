c8b9e4f8f931508b367b58b555ef92d7f4aee219
Sebastiano Tronto <sebastiano@tronto.net>
2024-10-07 11:14:42 +0200

    Remove -Werror compiler option

--- configure.sh
+++ configure.sh
@@ -85,7 +85,7 @@ validatethreads
 validatearch
 
 STD="-std=c11"
-WFLAGS="-pedantic -Wall -Wextra -Werror"
+WFLAGS="-pedantic -Wall -Wextra"
 WNOFLAGS="-Wno-unused-parameter -Wno-unused-function -Wno-unknown-pragmas"
 
 [ "$ARCH" = "AVX2" ] && AVX="-mavx2"
