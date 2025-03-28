50fedbdbf98bcf01bcd95259ac5a93ae108b3c68
Sebastiano Tronto <sebastiano@tronto.net>
2025-03-28 16:48:03 +0100

    Make invcoord_co test more comprehensive

--- test/079_invcoord_co/invcoord_co_tests.c
+++ test/079_invcoord_co/invcoord_co_tests.c
@@ -1,28 +1,34 @@
 #include "../test.h"
 
+#define POW_3_7 2187
+
 int64_t coord_co(cube_t);
 cube_t invcoord_co(int64_t);
 
 void run(void) {
-	char str[STRLENMAX];
 	cube_t cube;
-	int64_t coord;
+	int64_t coord, coord2;
 
-	fgets(str, STRLENMAX, stdin);
-	coord = atoll(str);
+	/* Test all possible values for CO coordinate */
+	for (coord = 0; coord < POW_3_7; coord++) {
+		cube = invcoord_co(coord);
 
-	cube = invcoord_co(coord);
+		if (!isconsistent(cube)) {
+			printf("Not consistent\n");
+			return;
+		}
+		if (!issolvable(cube)) {
+			printf("Not solvable\n");
+			return;
+		}
 
-	if (!isconsistent(cube)) {
-		printf("Not consistent\n");
-		return;
+		coord2 = coord_co(cube);
+		if (coord != coord2) {
+			printf("Error: invcoord of %" PRId64
+			    " returns %" PRId64 "\n", coord, coord2);
+			return;
+		}
 	}
-	if (!issolvable(cube)) {
-		printf("Not solvable\n");
-		return;
-	}
 
-	coord = coord_co(cube);
-
-	printf("%" PRId64 "\n", coord);
+	printf("All good\n");
 }
